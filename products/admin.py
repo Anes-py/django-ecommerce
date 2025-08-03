ad0f3354d091a7ebe857279dfbad8101781f56a6
Anes-py <AnesDevpy@gmail.com>
2025-08-03 21:09:17 +0330

    Improve ProductAdmin config with ordering, filters and prepopulated slug

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+        'id',
+        'price',
+        'stock',
+        'status',
+        'is_active',
+        'created_at',
+        'updated_at',
+    ]
+    readonly_fields = ['created_at', 'updated_at']
+    list_display_links = ['name']
+    list_per_page = 20
+    list_max_show_all = 30
+    list_editable = ['status', 'is_active']
+    list_filter = ['status', 'is_active', 'category']
+    autocomplete_fields = ['category', 'brand']
+    prepopulated_fields = {"slug": ("name",)}
+    search_fields = ['name', 'short_description']
+    ordering = ['-created_at', 'stock']
