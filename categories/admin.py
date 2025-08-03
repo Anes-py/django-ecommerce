9b1005572f401c7afa3d94825a3cd4d98c08601b
Anes-py <AnesDevpy@gmail.com>
2025-08-03 15:31:18 +0330

    create brand model admin

--- categories/admin.py
+++ categories/admin.py
@@ -1,15 +1,27 @@
 from django.contrib import admin
 
-from .models import Category
+from .models import Category, Brand
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['name',]
-    list_display_links = ['name',]
+    list_display = ['name']
+    list_display_links = ['name']
     list_filter = ['name', 'parent']
     search_fields = ['name', 'parent__name']
     ordering = ['name']
     autocomplete_fields = ['parent']
     prepopulated_fields = {'slug': ('name',)}
+    list_per_page = 20
+    list_max_show_all = 30
+
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    list_display_links = ['name']
+    list_filter = ['name']
+    search_fields = ['name', 'description']
+    prepopulated_fields = {'slug': ('name',)}
+    ordering = ['name']
     list_per_page = 20
     list_max_show_all = 30
