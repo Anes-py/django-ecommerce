5c79349a6428285be22ec5679ad50b66e6072040
Anes-py <AnesDevpy@gmail.com>
2025-08-03 15:04:32 +0330

    update

--- config/settings.py
+++ config/settings.py
@@ -10,19 +10,16 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
-
 from pathlib import Path
 from environs import Env
 
-env =Env()
+env = Env()
 env.read_env()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
 ALLOWED_HOSTS = []
