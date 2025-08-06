71f3e3024060e464ce20a000dfbab3c2dc1f8d2e
Anes-py <AnesDevpy@gmail.com>
2025-08-06 13:23:52 +0330

    Add inlines for banners in SiteSettings admin and register SiteSettingsAdmin

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import CustomUser
+from .models import *
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 
 @admin.register(CustomUser)
@@ -13,3 +13,34 @@ class CustomUserAdmin(UserAdmin):
         (None, {'fields':('email',)}),
     )
     fieldsets = UserAdmin.fieldsets
+
+
+class SliderBannerInline(admin.TabularInline):
+    model = SliderBanners
+    extra = 1
+    max_num = 10
+
+
+class SideBannerInline(admin.TabularInline):
+    model = SideBanners
+    extra = 2
+    min_num = 2
+    max_num = 2
+
+
+class MiddleBannerInline(admin.TabularInline):
+    model = MiddleBanners
+    extra = 2
+    min_num = 2
+    max_num = 2
+
+
+@admin.register(SiteSettings)
+class SiteSettingsAdmin(admin.ModelAdmin):
+    list_display = ['site_name']
+    list_display_links = ['site_name']
+    inlines = [
+        SliderBannerInline,
+        SideBannerInline,
+        MiddleBannerInline,
+    ]
