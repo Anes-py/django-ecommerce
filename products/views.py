cf7c54a16ef7a92059cedba690e70f17c1402985
Anes-py <AnesDevpy@gmail.com>
2025-08-06 15:49:40 +0330

    add HomeView with discounted, newest products and top categories context

--- products/views.py
+++ products/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import Product
+from categories.models import Category
+
+class HomeView(generic.TemplateView):
+    template_name = 'products/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'discounted_products':Product.objects.with_discount(),
+            'newest_products':Product.objects.newest(),
+            'top_categories':Category.objects.only('image', 'slug').prefetch_related("children")\
+        .select_related("parent").filter(parent__isnull=True)[:6] # demo
+        })
+        return context
