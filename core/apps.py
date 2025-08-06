2ac710f3d94e859b86bbcde02e4ccefaacd1bcfe
Anes-py <AnesDevpy@gmail.com>
2025-08-06 13:35:28 +0330

    customize Django admin site titles and headers

--- core/apps.py
+++ core/apps.py
@@ -1,7 +1,13 @@
 from django.apps import AppConfig
+from django.contrib import admin
 from django.utils.text import gettext_lazy as _
 
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
     verbose_name = _("core")
+
+    def ready(self):
+        admin.site.site_header = _('Store Admin Panel')
+        admin.site.site_title = _('Store Admin')
+        admin.site.index_title = _('Welcome to the Store Admin Panel')
