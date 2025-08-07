749cf61848518948aaa1832d5d682af5e7e4a4cb
Anes-py <AnesDevpy@gmail.com>
2025-08-07 12:14:27 +0330

    add core context processor for, categories, brand and site settings obj

---
+++ core/context_processors.py
@@ -0,0 +1,19 @@
+from django.db.models import Prefetch
+from django.conf import settings
+
+from categories.models import Category, Brand
+from .models import SiteSettings
+
+
+def site_settings(request):
+    queryset = SiteSettings.objects.first()
+    return {
+        'site_settings': queryset,
+        'categories': Category.objects.prefetch_related(
+            Prefetch(
+                "children",
+                Category.objects.prefetch_related("children")
+            )
+        ).filter(parent__isnull=True),
+        'brands' : Brand.objects.all(),
+    }
