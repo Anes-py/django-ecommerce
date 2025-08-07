782981ac5aa2d90a255b2c1e1b7b086d6103a655
Anes-py <AnesDevpy@gmail.com>
2025-08-07 17:03:06 +0330

    Optimize home page by using cached categories

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.views import generic
 
 from core.models import SliderBanners, SideBanners, MiddleBanners
+from core.services.site_cache import get_site_context
 from categories.models import Category
 
 from .models import Product
@@ -11,14 +12,13 @@ class HomeView(generic.TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        site_context = get_site_context(self.request)
         context.update({
             'slider_banners': SliderBanners.objects.all(),
             'side_banners': SideBanners.objects.all(),
             'middleBanners':MiddleBanners.objects.all(),
             'discounted_products':Product.objects.with_discount(),
             'newest_products':Product.objects.newest(),
-            'top_categories':Category.objects.only('image', 'slug', 'parent').prefetch_related("children")\
-        .select_related("parent").filter(parent__isnull=True)[:6] # demo
-
+            'top_categories':site_context['categories'].filter(parent__isnull=True)[:6]
         })
         return context
