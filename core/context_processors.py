690a33d60ec6e38c5708fb38e88592054e87ebc1
Anes-py <AnesDevpy@gmail.com>
2025-08-07 14:57:42 +0330

    add cache to core context processor for better perfomance

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,19 +1,21 @@
+from django.core.cache import cache
 from django.db.models import Prefetch
-from django.conf import settings
 
 from categories.models import Category, Brand
 from .models import SiteSettings
 
 
 def site_settings(request):
-    queryset = SiteSettings.objects.first()
-    return {
-        'site_settings': queryset,
-        'categories': Category.objects.prefetch_related(
-            Prefetch(
-                "children",
-                Category.objects.prefetch_related("children")
-            )
-        ).filter(parent__isnull=True),
-        'brands' : Brand.objects.all(),
-    }
+    cached_data = cache.get('site_context_data')
+    if not cached_data:
+        cached_data = {
+            'site_settings':SiteSettings.objects.first(),
+
+            'categories':Category.objects.prefetch_related(
+                Prefetch('children', queryset=Category.objects.only('id', 'name', 'slug', 'image'))
+            ).only('id', 'name', 'slug', 'image'),
+
+            'brands':Brand.objects.all().only('id', 'name'),
+        }
+        cache.set('site_context_data', cached_data, 3600) # 1 hour = 3600 seconds
+    return cached_data
