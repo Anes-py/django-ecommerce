9eb266217089fc57e88d94a118a6b2257d1afbb8
Anes-py <AnesDevpy@gmail.com>
2025-08-08 13:41:12 +0330

    Add exception handling for banners when SiteSetting object does not exist

--- products/views.py
+++ products/views.py
@@ -1,6 +1,6 @@
 from django.views import generic
 
-from core.models import SliderBanners, SideBanners, MiddleBanners
+from core.models import SliderBanners, SideBanners, MiddleBanners, SiteSettings
 from core.services.site_cache import get_site_context
 from categories.models import Category
 
@@ -14,11 +14,16 @@ class HomeView(generic.TemplateView):
         context = super().get_context_data(**kwargs)
         site_context = get_site_context(self.request)
         context.update({
-            'slider_banners': SliderBanners.objects.all(),
-            'side_banners': SideBanners.objects.all(),
-            'middleBanners':MiddleBanners.objects.all(),
             'discounted_products':Product.objects.with_discount(),
             'newest_products':Product.objects.newest(),
             'top_categories':site_context['categories'].filter(parent__isnull=True)[:6]
         })
+        if SiteSettings.objects.all().first():
+            context.update({
+            'slider_banners': SliderBanners.objects.all(),
+            'side_banners': SideBanners.objects.all(),
+            'middleBanners':MiddleBanners.objects.all(),
+            })
         return context
+
+
