d92a11c6aec1baae7cf197eafafdc43dd1f91858
Anes-py <AnesDevpy@gmail.com>
2025-08-12 11:11:20 +0330

    add cart model

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils.text import gettext_lazy as _
+from django.utils import timezone
 
-# Create your models here.
+class Cart(models.Model):
+    user = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='cart',
+        null=True,
+        blank=True,
+        verbose_name=_('user'),
+    )
+    session_key = models.CharField(_('session_key'), max_length=40)
+    created_at = models.DateTimeField(_('created at'), default=timezone.now)
+    updated_at = models.DateTimeField(_('updated at'), auto_now=True)
+
+    class Meta:
+        verbose_name = _('Cart')
+        verbose_name_plural = _('Carts')
+
+    def __str__(self):
+        return f"{_('cart of')} {self.user.username if self.user else 'Guest'}  | {self.session_key}"
