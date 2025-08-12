2811684a9de046f3c565a6b62e87861cc21e6cd5
Anes-py <AnesDevpy@gmail.com>
2025-08-12 12:31:53 +0330

    add cart item model

--- cart/models.py
+++ cart/models.py
@@ -3,6 +3,8 @@ from django.contrib.auth import get_user_model
 from django.utils.text import gettext_lazy as _
 from django.utils import timezone
 
+from products.models import Product
+
 class Cart(models.Model):
     user = models.ForeignKey(
         get_user_model(),
@@ -22,3 +24,26 @@ class Cart(models.Model):
 
     def __str__(self):
         return f"{_('cart of')} {self.user.username if self.user else 'Guest'}  | {self.session_key}"
+
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(
+        Cart,
+        on_delete=models.CASCADE,
+        related_name="items",
+        verbose_name=_("items")
+    )
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name="products",
+        verbose_name=_("product"),
+    )
+    quantity = models.PositiveIntegerField(_("quantity"), default=1)
+    color = models.CharField(_("color"), max_length=10, null=True, blank=True)
+    size = models.CharField(_("size"), max_length=55, null=True, blank=True)
+
+    class Meta:
+        verbose_name = _("Cart Item")
+        verbose_name_plural = _("Cart Items")
