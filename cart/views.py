0530fb272cea66865bc72fd750109d8cf06e818e
Anes-py <AnesDevpy@gmail.com>
2025-08-13 15:54:23 +0330

    add cart model form

--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,8 @@
 from django.db.models import Prefetch
 from django.views import generic
+from django.shortcuts import get_object_or_404
 
+from products.models import Product
 from .models import Cart, CartItem
 
 
@@ -20,3 +22,17 @@ class CartDetailView(generic.DetailView):
                 Prefetch('items', queryset=CartItem.objects.select_related('product', 'product__discount'))
             ).get_or_create(session_key=session_key)
         return cart
+
+
+class AddToCartView(generic.View):
+    def post(self):
+        product_id = self.kwargs.get('product_id')
+        product = get_object_or_404(Product, id=product_id)
+        if self.request.user.is_authenticated:
+            cart, _ = Cart.objects.get_or_create(user=self.request.user)
+        else:
+            if not self.request.session.session_key:
+                self.request.session.create()
+            cart, _ = Cart.objects.get_or_create(session_key=self.request.session.session_key)
+
+        cart_item, created = CartItem.objects.get_or_create()
