468670735bc4996638dfe275824d9e9754d364cc
Anes-py <AnesDevpy@gmail.com>
2025-08-25 14:21:42 +0330

    add a demo of user dashboard

--- core/views.py
+++ core/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from orders.models import Order
+
+
+def user_dashboard_view(request):
+    context = {
+    'pending_orders':Order.objects.filter(
+        status=Order.OrderStatus.PENDING_PAYMENT,
+    ),
+    'paid_orders':Order.objects.filter(
+        status=Order.OrderStatus.PAID,
+    ),
+    'refunded_orders':Order.objects.filter(
+        status=Order.OrderStatus.REFUNDED,
+    ),
+    'fulfilled_orders':Order.objects.filter(
+        status=Order.OrderStatus.FULFILLED,
+    ),
+    'delivered_orders':Order.objects.filter(status=Order.OrderStatus.DELIVERED)
+    }
+
+
+    return render(request, 'core/user_dashboard.html', context=context)
+
