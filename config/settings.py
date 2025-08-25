11194b9d6a78fecb46ae0668fed81c4239598ef6
Anes-py <AnesDevpy@gmail.com>
2025-08-25 15:19:24 +0330

    update static file setting, add login template

--- config/settings.py
+++ config/settings.py
@@ -131,6 +131,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
